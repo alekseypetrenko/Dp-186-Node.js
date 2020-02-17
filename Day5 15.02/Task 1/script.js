0cd5554ed67473156d236244b50912d82394b0a5
Aleksey Petrenko <alekseypetrenko@users.noreply.github.com>
2020-02-17 23:01:12 +0200

    task 1, my methods

--- Day5 15.02/Task 1/script.js
+++ Day5 15.02/Task 1/script.js
@@ -1,16 +1,34 @@
-//console.log(["Яблоко", "Банан", "Ананас"].map(el => el[0]));
-//С помощью reduce, перепишите реализацию следующего кода:
+// ----- Напишите свои функции-аналоги методов массивов используя только свойства массивов: pop, push, shift, unshift, concat
 
-// 1) ["Яблоко", "Банан", "Ананас"].map(el => el[0])
-// => 
+let arr = [1, 2, 3, 4, 5];
 
+function myPop(arr){
+    let lastEl = arr[arr.length-1];
+    arr.length = arr.length -1;
+    return lastEl;
+}
 
+function myPush(arr, ...rest){    
+    for (let i = 0; i < rest.length; i++) {
+        arr[arr.length] = rest[i];
+    }
+    return arr;
+}
 
-console.log(["Яблоко", "Банан", "Ананас"].reduce((acc, cur)=> {
+function myShift(arr){
+    let firstEl = arr[0];
 
-    
-    acc.push(cur[0])    
-    return acc
-}, [])
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i+1];
+    }
+    arr.length = arr.length - 1
+    return firstEl;
+}
 
-);
+function myUnshift(arr, ...rest){
+
+}
+
+function myConcat(arr, ...rest){
+
+}
