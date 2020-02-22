05bd438dc411b358c42339cfeeee0aeb7354b607
Aleksey Petrenko <alekseypetrenko@users.noreply.github.com>
2020-02-22 19:55:31 +0200

    changes

--- JS Core - February/Day 10 HW (tic-tac, battle ship, random, user)/Task 1 (tic-tac)/js.js
+++ JS Core - February/Day 10 HW (tic-tac, battle ship, random, user)/Task 1 (tic-tac)/js.js
@@ -1,10 +1,5 @@
-    let board0 = [
+    let board1= [
         [1, 2, 1],
-        [2, 1, 2],
-        [2, 2, 1]
-    ]
-    let board1 = [
-        [1, 1, 1],
         [2, 1, 2],
         [2, 2, 1]
     ]
@@ -16,12 +11,14 @@
     let boardMinus1 = [
         [1, 2, 1],
         [2, 1, 0],
+        [2, 2, 0]
+    ]
+    let boardO = [
+        [2, 2, 1],
+        [1, 1, 2],
         [2, 2, 1]
     ]
     function checkBoard(board){
-        let row0 = board[0];
-        let row1 = board[1];
-        let row2 = board[2];
         let col0 = []
         let col1 = []
         let col2 = []
@@ -33,36 +30,52 @@
         diagonal1.push(board[0][0], board[1][1], board[2][2]);
         diagonal2.push(board[0][2], board[1][1], board[2][0]);
 
-        if( checkX(row0) || checkX(row1) || checkX(row2) || checkX(col0) || checkX(col1) || checkX(col2)) {
+        if( checkX(board) || checkXCol(col0) || checkXCol(col1) || checkXCol(col2) || checkXCol(diagonal1) || checkXCol(diagonal2)) {
             console.log(1);
-        } else if(checkO(row0) || checkO(row1) || checkO(row2) || checkO(col0) || checkO(col1) || checkO(col2)){
+        } else if(checkOCol(board) || checkOCol(col0) || checkOCol(col1) || checkOCol(col2) || checkOCol(diagonal2) || checkOCol(diagonal2)){
             console.log(2);
-            
         } else if (
-            checkZero(row0) || checkZero(row1) || checkZero(row2) || checkZero(col0) || checkZero(col1) || checkZero(col2)
-            && !(checkX(row0) || checkX(row1) || checkX(row2) || checkX(col0) || checkX(col1) || checkX(col2)) 
-            && !(checkO(row0) || checkO(row1) || checkO(row2) || checkO(col0) || checkO(col1) || checkO(col2))){
+            checkZero(board) || checkZeroCol(col0) || checkZeroCol(col1) || checkZeroCol(col2) || checkZeroCol(diagonal1) || checkZeroCol(diagonal2)
+            && !(checkX(board) || checkXCol(col0) || checkXCol(col1) || checkXCol(col2) || checkXCol(diagonal1) || checkXCol(diagonal2)) 
+            && !(checkO(board) || checkOCol(col0) || checkOCol(col1) || checkOCol(col2) || checkOCol(diagonal2) || checkOCol(diagonal2))){
                 console.log(-1);
         } else {
             console.log(0);
-            
         }
-
-        
     }
     
     function checkX(arr){
+        for (let i = 0; i < arr.length; i++){
+            return arr[i].every(el => el == 1)
+        }
+    }    
+    
+    function checkXCol(arr){
         return arr.every(el => el == 1)
     }
+    
     function checkO(arr){
+        for (let i = 0; i < arr.length; i++){
+            return arr[i].every(el => el == 2)
+        }  
+    }    
+    
+    function checkOCol(arr){
         return arr.every(el => el == 2)
     }
+
     function checkZero(arr){
+        for (let i = 0; i < arr.length; i++){
+            return arr[i].every(el => el == 1)
+        }
+    }
+    
+    function checkZeroCol(arr){
         return arr.some(el => el == 0)
     }
 
-checkBoard(board0)
 checkBoard(board1)
 checkBoard(board2)
 checkBoard(boardMinus1)
+checkBoard(boardO)
 
