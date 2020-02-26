986355b75c8cc15841929eeb708a6bb77f24b30b
Aleksey Petrenko <alekseypetrenko@users.noreply.github.com>
2020-02-26 09:33:57 +0200

    task 2 is fixed

--- JS Core - February/Day_12 HW (binary tree, localStorage)/Task2/script.js
+++ JS Core - February/Day_12 HW (binary tree, localStorage)/Task2/script.js
@@ -1,11 +1,11 @@
 
-// let list = document.querySelector("ul")
-// let todos;
+let list = document.querySelector("ul")
+let todos;
 
-// function addToLocal() {
-//     todos = list.innerHTML
-//     localStorage.setItem("todos", todos);
-// }
+function addToLocal() {
+    todos = list.innerHTML
+    localStorage.setItem("todos", todos);
+}
 
 function addNewIitem() {
     let newItem = document.getElementById("toDoItem").value;
@@ -15,21 +15,21 @@ function addNewIitem() {
         alert("Enter somethig...")
     } else {
         li.appendChild(document.createTextNode(newItem))
-        items.appendChild(li);
+        items.prepend(li);
     }
 
 
-    // addToLocal(); 
-    // if(localStorage.getItem("todos")){
-    //     list.innerHTML = localStorage.getItem("todos")
-    // }
-    
-    //const arr = JSON.parse(localStorage.getItem("todos") || []);
-    //arr.push(newItem);
-
-    let  arr = JSON.parse(localStorage.getItem("todos") || []);
-    arr.push(newItem)
-    localStorage.setItem("todos", JSON.stringify(arr));
-
     document.getElementById("toDoItem").value = "";
+    addToLocal(); 
+
+    // WHY - Unexpectd end of JSON input ?????
+    // const arr = JSON.parse(localStorage.getItem("todos") || []);
+    // arr.push(newItem);
+    // let  arr = JSON.parse(localStorage.getItem("todos") || []);
+    // arr.push(newItem)
+    // localStorage.setItem("todos", JSON.stringify(arr));
+}
+
+if(localStorage.getItem("todos")){
+    list.innerHTML = localStorage.getItem("todos")
 }
