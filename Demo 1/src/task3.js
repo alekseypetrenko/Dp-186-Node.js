57c95f4d7240134b1e4a65a74ec89837bd4c8c0d
Aleksey Petrenko <alekseypetrenko@users.noreply.github.com>
2020-02-29 11:03:11 +0200

    3

--- Demo 1/src/task3.js
+++ Demo 1/src/task3.js
@@ -10,15 +10,11 @@ function sortTriangles(arr) {
                 return { status: "failed", reason: "Вершины треугольников не могут иметь одинаковое имя" };
             }
         }
-        let values = Object.values(myArr[i]);
-        values.shift();
-        // проверяем является ли фигура треугольником
-        if (values[0] + values[1] <= values[2] || values[1] + values[2] <= values[0] || values[2] + values[1] <= values[0]) {
-            return { status: "failed", reason: 'Ошибка, сумма длин каждых двух сторон должна быть больше длины третьей стороны.' };
-        }
+
         let splittedVert = myArr[i].vertices.toLowerCase().split("");
         for (vert in myArr[i]) {
             if (vert != "vertices") {
+                myArr[i][vert] = Math.abs(myArr[i][vert]);
                 if (typeof myArr[i][vert] != "number" || myArr[i][vert] == 0) {
                     return { status: "failed", reason: "Вершины должны быть числами больше нуля" };
                 }
@@ -27,18 +23,33 @@ function sortTriangles(arr) {
                 }
             }
         }
-
+        let values = Object.values(myArr[i]);
+        values.shift();
+        // проверяем является ли фигура треугольником
+        if (values[0] + values[1] <= values[2] || values[1] + values[2] <= values[0] || values[2] + values[1] <= values[0]) {
+            return { status: "failed", reason: 'Ошибка, сумма длин каждых двух сторон должна быть больше длины третьей стороны.' };
+        }
     }
 
-    // let s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    let areaCalc = myArr.map(triangle => {
+        let arr = []
+        for (let vert in triangle) {
+            if (vert != "vertices") {
+                arr.push(triangle[vert])
+            }
+        }
+        let p = (arr[0] + arr[1] + arr[2]) / 2
+        let area = Math.sqrt(p * (p - arr[0]) * (p - arr[1]) * (p - arr[2]))
+        return { vertices: triangle.vertices, area }
+    })
 
-    return myArr;
-
+    return areaCalc.sort((a, b) => a.area - b.area )
+                    .map(el => el.vertices);
 }
 
 
 let triangles = [
-    { vertices: "BCA", b: 35, c: 20, a: 40 },
+    { vertices: "BCA", b: 35, c: -20, a: 40 },
     { vertices: "ABF", a: 1, b: 2.2, f: 2.2 },
     { vertices: "EDF", e: 40, d: 30, f: 65 },
     { vertices: "ABC", a: 10, b: 20, c: 20 },
