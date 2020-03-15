4993432473cc19a5cc6eb7bbe734f93f8f7d149d
Oleksii Petrenko <opetr@dp223.softservecom.com>
2020-03-15 21:53:50 +0200

    123

--- JS Core - March/Day_4 HW Animal shop/app/components/animal/AnimalModel.js
+++ JS Core - March/Day_4 HW Animal shop/app/components/animal/AnimalModel.js
@@ -2,7 +2,6 @@ export class AnimalModel {
     link = "data/data.json"
     constructor(callback) {
         this.handleLoad = callback;
-        // this.convertDate = callback2;
     }
 
     getAnimals() {
@@ -10,7 +9,6 @@ export class AnimalModel {
         xhr.addEventListener("load", () => {
             this.animals = JSON.parse(xhr.responseText);
             this.handleLoad(this.animals);
-            // this.convertDate(this.animals);
         })
         xhr.open("GET", this.link);
         xhr.send();
